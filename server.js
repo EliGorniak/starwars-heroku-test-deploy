b984a812840b681170658b176f59cc19af9ce730
Elianice Gorniak <eligorniak@gmail.com>
2019-09-18 12:40:42 -0500

    fixed PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.enc.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -75,7 +75,7 @@ app.post("/api/characters", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newCharacter = req.body;
-  console.log("newCharacter", newCharacter)
+  console.log("newCharacter", newCharacter);
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
